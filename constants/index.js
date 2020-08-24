399bae39b7ce6a88036bc7cbc0b09863e3844879
ThinkerZhang <15521090614@163.com>
2020-08-24 21:39:34 +0800

    refactor(utils): add openSetting function and fix util.js

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,19 @@
 const { statusBarHeight } = wx.getSystemInfoSync()
+const scopeMappings = {
+  'scope.userInfo': '用户信息', // 对应接口 wx.getUserInfo
+  'scope.userLocation': '地理位置', // 对应接口 wx.getLocation, wx.chooseLocation
+  'scope.userLocationBackground': '后台定位', // wx.startLocationUpdateBackground
+  'scope.address': '通讯地址', // 对应接口 wx.chooseAddress
+  'scope.invoiceTitle': '发票抬头', // 对应接口 wx.chooseInvoiceTitle
+  'scope.invoice': '获取发票', // 对应接口 wx.chooseInvoice
+  'scope.werun': '微信运动步数', // 对应接口 wx.getWeRunData
+  'scope.record': '录音功能', // 对应接口 wx.startRecord 
+  'scope.writePhotosAlbum': '保存到相册', // wx.saveImageToPhotosAlbum, wx.saveVideoToPhotosAlbum
+  'scope.camera': '摄像头' // 对应[camera]((camera)) 组件
+}
 
 export default {
   navigationBar: 44 + statusBarHeight, // 导航栏高度
   statusBar: statusBarHeight, // 状态栏高度
+  scopeMappings, // 用户授权设置信息
 }
--- utils/util.js
+++ utils/util.js
@@ -1,37 +1,85 @@
+import constants from '../constants/index'
+
 /**
- * 二次封装showToast
- * @param {*} message
- */ 
-const showToast = message => {
-  wx.showToast({
-    title: message || '系统异常，请稍后再试~',
-    icon: 'none'
+ * 二次封装navigateBack
+ */
+const navigateBack = () => {
+  wx.navigateBack({
+    fail: (err) => {
+      console.log('navigateBack fail', err)
+      wx.reLaunch({
+        url: '/pages/index/index'
+      })
+    }
+  })
+}
+
+/**
+ * 二次封装openSetting
+ * @param scope 用户授权设置信息属性（默认为scope.writePhotosAlbum）
+ */
+const openSetting = (scope = 'scope.writePhotosAlbum') => {
+  return new Promise((resolve, reject) => {
+    const content = `您拒绝了“${constants.scopeMappings[scope]}”授权\n请点击确定按钮重新打开授权`
+    showModal('提示', content).then(res => {
+      // console.log(res)
+      wx.openSetting({
+        success: (res) => {
+          console.log('openSetting success', res)
+          if (res.authSetting[scope]) {
+            resolve(res)
+          } else {
+            reject(res)
+          }
+        },
+        fail: (err) => {
+          console.log('openSetting fail', err)
+          reject(err)
+        }
+      })
+    }).catch(err => {
+      console.log(err)
+    })
   })
 }
 
 /**
  * 二次封装showModal
- * @param {*} message 
+ * @param title 提示的标题
+ * @param content 提示的内容
+ * @param showCancel 是否显示取消按钮
  */
-const showModal = message => {
-  wx.showModal({
-    title: '提示',
-    content: message,
-    showCancel: false,
-    success: (res) => {
-      // console.log('showModal success', res)
-      if (res.confirm) {
-        // 正常返回上一页，若返回失败则直接回到首页
-        wx.navigateBack({
-          fail: (err) => {
-            console.log('navigateBack fail', err)
-            wx.reLaunch({
-              url: '/pages/index/index'
-            })
-          }
-        })
+const showModal = (title = '', content = '', showCancel = true) => {
+  return new Promise((resolve, reject) => {
+    wx.showModal({
+      title,
+      content,
+      showCancel,
+      success: (res) => {
+        // console.log('showModal success', res)
+        if (res.confirm) {
+          resolve(res)
+        } else {
+          reject(res)
+        }
+      },
+      fail: (err) => {
+        console.log('showModal fail', err)
+        reject(err)
       }
-    }
+    })
+  })
+}
+
+/**
+ * 二次封装showToast
+ * @param title 提示的内容
+ * @param icon 图标
+ */
+const showToast = (title = '系统异常，请稍后再试~', icon = 'none') => {
+  wx.showToast({
+    title,
+    icon
   })
 }
 
@@ -59,8 +107,10 @@ const throttle = (fn, delay = 5000) => {
 }
 
 module.exports = {
-  showToast,
+  navigateBack,
+  openSetting,
   showModal,
+  showToast,
   debounce,
   throttle
 }
