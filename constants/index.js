93b99fdbd15b7b3df5036abe0011006b76442b35
ThinkerZhang <15521090614@163.com>
2020-09-28 11:36:32 +0800

    fix: update index.js

--- constants/index.js
+++ constants/index.js
@@ -3,9 +3,9 @@ const scopeMappings = {
   'scope.userInfo': '用户信息', // 对应接口 wx.getUserInfo
   'scope.userLocation': '地理位置', // 对应接口 wx.getLocation, wx.chooseLocation
   'scope.userLocationBackground': '后台定位', // wx.startLocationUpdateBackground
-  'scope.address': '通讯地址', // 对应接口 wx.chooseAddress
-  'scope.invoiceTitle': '发票抬头', // 对应接口 wx.chooseInvoiceTitle
-  'scope.invoice': '获取发票', // 对应接口 wx.chooseInvoice
+  'scope.address': '通讯地址', // 对应接口 wx.chooseAddress(20200925无需获取用户授权)
+  'scope.invoiceTitle': '发票抬头', // 对应接口 wx.chooseInvoiceTitle(20200925无需获取用户授权)
+  'scope.invoice': '获取发票', // 对应接口 wx.chooseInvoice(20200925无需获取用户授权)
   'scope.werun': '微信运动步数', // 对应接口 wx.getWeRunData
   'scope.record': '录音功能', // 对应接口 wx.startRecord 
   'scope.writePhotosAlbum': '保存到相册', // wx.saveImageToPhotosAlbum, wx.saveVideoToPhotosAlbum
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,34 +1,32 @@
 //index.js
-Component({
+Page({
   behaviors: [wx.$behaviors.pagination],
   data: {
     loading: true
   },
-  methods: {
-    getList(page = 1, limit = 10) {
-      return new Promise((resolve, reject) => {
-        wx.request({
-          url: 'https://brain.lololun.top/getList',
-          dataType: 'json',
-          data: {
-            page,
-            limit
-          },
-          success: (res) => {
-            resolve(res.data)
-          },
-          fail: (err) => {
-            reject(err)
-          }
-        })
+  getList(page = 1, limit = 10) {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: 'https://brain.lololun.top/getList',
+        dataType: 'json',
+        data: {
+          page,
+          limit
+        },
+        success: (res) => {
+          resolve(res.data)
+        },
+        fail: (err) => {
+          reject(err)
+        }
       })
-    },
-    async onLoad(options) {
-      const list = await this.getList()
-      this.setData({
-        loading: false,
-        list
-      })
-    }
+    })
+  },
+  async onLoad(options) {
+    const list = await this.getList()
+    this.setData({
+      loading: false,
+      list
+    })
   }
 })
