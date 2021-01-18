2e99982e6613e4929211e081989a8266942acb9c
ThinkerZhang <15521090614@163.com>
2021-01-18 16:05:46 +0800

    feat: remove rarely used plugins

--- app.js
+++ app.js
@@ -1,30 +1,4 @@
 //app.js
-
-/*
- * 数据打点
- */
-const { BxTracker } = require('./tracker/anka-tracker.min.js')
-const trackerConfig = require('./tracker/anka-tracker.config.js')
-const tracker = BxTracker.generateTrackerInstance(trackerConfig)
-tracker.asyncInitWithCommonData({
-  open_id: 'mock_open_id',
-  union_id: 'mock_union_id'
-}).then(() => {
-  console.log('初始化成功，开始执行打点任务')
-})
-
-/*
- * 调试工具
- */
-import weDebug from '@we-debug/core/libs/index'
-import vconsole from '@we-debug/plugin-vconsole'
-import navigate from '@we-debug/plugin-navigate'
-weDebug.use(vconsole)
-weDebug.use(navigate, [
-  { title: 'test页面跳转', desc: '点击右侧按钮前往test页面', url: '/pages/test/test?foo=bar' }
-])
-weDebug.init()
-
 import behaviors from './behaviors/index'
 import constants from './constants/index'
 import config from './config'
@@ -45,7 +19,7 @@ App({
     wx.$log = log
     wx.$util = util
     wx.$dayjs = dayjs
-    wx.$tracker = tracker
+
     // 监听小程序更新
     const updateManager = wx.getUpdateManager()
     updateManager.onUpdateReady(() => {
